22e98a38763dcf31d8d74c854aad611ef785b2cb
minami-taiga <373735656t@gmail.com>
2020-02-06 12:19:04 +0000

    topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/show'
+  get 'items/edit'
+  root to: "items#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
