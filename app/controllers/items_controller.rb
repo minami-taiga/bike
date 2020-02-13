be697da4db6299f607008803168413a92c3b1007
minami-taiga <373735656t@gmail.com>
2020-02-13 09:57:31 +0000

    fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,31 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @user = current_user
   end
 
   def show
   end
 
   def edit
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+      if @item.save
+       redirect_to items_path
+    else
+    render :new
+    end
+  end
+
+    private
+    def item_params
+        params.require(:item).permit(:bike_name, :image, :content)
   end
 end
