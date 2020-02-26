53cc8aa302d2f4f6840151325abc08f32e9cc9a4
minami-taiga <373735656t@gmail.com>
2020-02-26 08:17:12 +0000

    secret_keyを追加しました

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '4b861663e311c23176b390dc82ae3df39625e0dd00eaed2a27d5633b3f53cd658756331b8ed4cedaa32dbcf4ac5d061039573599ab2f0e7b15daf68a914631d5'
+
